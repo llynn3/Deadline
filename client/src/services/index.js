f3b1d113f4083e32345be02218fbda9ce6a6bbe0
llynn3 <lynnelu21@gmail.com>
2021-11-02 15:48:14 -0700

    - create react app - create lregister and login function in services/index.js

---
+++ client/src/services/index.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const apiURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000"
+    : "produrl";
+
+axios.defaults.withCredentials = true;
+
+export const defaultRoute = async () => {
+  try {
+    const response = await axios.get(apiURL);
+    return response.data;
+  } catch (error) {
+    console.error(error.message);
+  }
+} 
+
+export const register = async (newUser) => {
+  try {
+    const response = await axios.post(`${apiURL}/auth/register`, newUser);
+    return response.data;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+export const login = async (userInfo) => {
+  try {
+    const response = await axios.post(`${apiURL}/auth/login`, userInfo);
+    return response.data;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
