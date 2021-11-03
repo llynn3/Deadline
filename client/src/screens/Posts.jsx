aec990c38f390d96823cc4c3ff6d1356ce80fab1
llynn3 <lynnelu21@gmail.com>
2021-11-03 00:03:33 -0700

    add have to be logged in to see comments, needs to be fixed

--- client/src/screens/Posts.jsx
+++ client/src/screens/Posts.jsx
@@ -13,9 +13,17 @@ const Posts = (props) => {
         <section>
             {posts.map((post) => (
                 <div>
+                    {props.user ? (
+                        <>
                     <h3>{post.title}</h3>
                     <h4>{post.body}</h4>
                     <h5>{post.dog_breed}</h5>
+                    </>
+                    ) : (
+                        <>
+                        <Link to='/login'>Log in to view posts.</Link>
+                        </>
+                    )}
                     <section className="comment-container">
                         <Link to={`/comments/${post.id}`}>
                             Comments.
