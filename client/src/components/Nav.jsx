5585307ede91e7ba7366beed9b5c1dbae56b8b09
llynn3 <lynnelu21@gmail.com>
2021-11-04 23:21:10 -0700

    fix css for navbar

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,13 +1,17 @@
+import './Nav.css';
+
 import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/posts">Posts</Link>
-      <Link to="/new">New Post</Link>
+      <div className="nav-bar">
+      <Link to="/" style={{ color: '#000000' }}>Home</Link>
+      <Link to="/login" style={{ color: '#000000' }}>Login</Link>
+      <Link to="/register" style={{ color: '#000000' }}>Register</Link>
+      <Link to="/posts" style={{ color: '#000000' }}>Posts</Link>
+      <Link to="/new" style={{ color: '#000000' }}>New Post</Link>
+      </div>
     </nav>
   );
 };
