08ce648cb02fbfc2664edbb8400578e0450f62bb
llynn3 <lynnelu21@gmail.com>
2021-11-05 11:04:33 -0700

    set database for production

--- client/src/screens/Posts.jsx
+++ client/src/screens/Posts.jsx
@@ -3,54 +3,49 @@ import { Link, useHistory } from "react-router-dom";
 import { deletePost, getAllPosts } from "../services";
 
 const Posts = (props) => {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    const history = useHistory();
+  const history = useHistory();
 
-    useEffect(() => {
-        getAllPosts().then((fetchedPosts) => setPosts(fetchedPosts));
-    }, []);
-    // console.log(posts)
-    // console.log(props.user.username)
+  useEffect(() => {
+    getAllPosts().then((fetchedPosts) => setPosts(fetchedPosts));
+  }, []);
+  // console.log(posts)
+  // console.log(props.user.username)
 
-    const handleDelete = async (postId) => {
-        await deletePost(postId)
+  const handleDelete = async (postId) => {
+    await deletePost(postId);
 
-        history.push("/posts")
-    }
+    history.push("/posts");
+  };
 
-
-    return (
-        <section>
-            {posts.map((post) => (
-                <div>
-                    {props.user ? (
-                        <>
-                    <h3>{props.user.username}</h3>
-                    <h4>{post.body}</h4>
-                    <h5>{post.dog_breed}</h5>
-                    </>
-                    ) : (
-                        <>
-                        <Link to='/login'>Log in to view posts.</Link>
-                        </>
-                    )}
-                                        {/* <Posts key={posts.id} post={post} /> */}
-                    <section className="comment-container">
-                        <Link to={`/posts/edit/${post.id}`}>
-                            Edit post.
-                            </Link>
-                        <Link to={`/posts/${post.id}`}>
-                            <button onClick={() => handleDelete(post.id)}>Delete.</button>
-                            </Link>
-                        <Link to={`/comments/${post.id}`}>
-                            Comments.
-                        </Link>
-                    </section>
-                </div>
-            ))}
-        </section>
-    );
+  return (
+    <section>
+      {posts?.map((post) => (
+        <div>
+          {props.user ? (
+            <>
+              <h3>{props.user.username}</h3>
+              <h4>{post.body}</h4>
+              <h5>{post.dog_breed}</h5>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Log in to view posts.</Link>
+            </>
+          )}
+          {/* <Posts key={posts.id} post={post} /> */}
+          <section className="comment-container">
+            <Link to={`/posts/edit/${post.id}`}>Edit post.</Link>
+            <Link to={`/posts/${post.id}`}>
+              <button onClick={() => handleDelete(post.id)}>Delete.</button>
+            </Link>
+            <Link to={`/comments/${post.id}`}>Comments.</Link>
+          </section>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default Posts;
