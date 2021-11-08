266948170e8fa4fc6109b2316f3ace3e54abb6b5
llynn3 <lynnelu21@gmail.com>
2021-11-07 23:50:54 -0800

    add home css screen

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,9 +1,14 @@
+import welcome from "../images/pupeyes.jpeg";
+import './Home.css';
+
 const Home = (props) => {
-  // const [user, setUser] = props.fields;
+
 
   return (
-    <section>
-      {/* <h4 style={{color: "#4895ef"}}>Hi, {user.username}!</h4> */}
+    <section className="home">
+      <img className="dog-sign" src={welcome} alt="welcome"/>
+      Welcome home 
+      <p>{props.user}</p>
     </section>
   );
 };
