9cf457aeb13a2536aca694179840cb24ef39d853
llynn3 <lynnelu21@gmail.com>
2021-11-07 16:49:43 -0800

    add footer

---
+++ client/src/screens/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            Lynne @ 2021 | GitHub 
+        </div>
+    );
+};
+
+export default Footer;
